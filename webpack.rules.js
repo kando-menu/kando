eb630dd8e29c1116e2375af6772c1660078e4b4d
Simon Schneegans <code@simonschneegans.de>
2023-05-03 06:11:46 +0200

    :sparkles: Fix formatting

--- webpack.rules.js
+++ webpack.rules.js
@@ -6,26 +6,26 @@ module.exports = [
   {
     // We're specifying native_modules in the test because the asset relocator loader
     // generates a "fake" .node file which is really a cjs file.
-    test: /native_modules[/\\].+\.node$/,
-    use: 'node-loader',
+    test : /native_modules[/\\].+\.node$/,
+    use : 'node-loader',
   },
   {
-    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
-    parser: {amd: false},
-    use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
+    test : /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+    parser : {amd : false},
+    use : {
+      loader : '@vercel/webpack-asset-relocator-loader',
+      options : {
+        outputAssetBase : 'native_modules',
       },
     },
   },
   {
-    test: /[/\\]build[/\\]Release[/\\].+\.node$/,
-    parser: {amd: false},
-    use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
+    test : /[/\\]build[/\\]Release[/\\].+\.node$/,
+    parser : {amd : false},
+    use : {
+      loader : '@vercel/webpack-asset-relocator-loader',
+      options : {
+        outputAssetBase : 'native_modules',
       },
     },
   },
