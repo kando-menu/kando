0c57fd04f830d50e97c36ae593769d866f6f885c
Simon Schneegans <code@simonschneegans.de>
2023-05-05 21:10:19 +0200

    :wrench: Do not use exclamation marks

--- src/backend/index.js
+++ src/backend/index.js
@@ -17,7 +17,7 @@ let Backend;
 if (os.platform() === 'linux') {
 
   console.log(`Running on Linux (${process.env.XDG_CURRENT_DESKTOP} on ${
-    process.env.XDG_SESSION_TYPE})!`);
+    process.env.XDG_SESSION_TYPE}).`);
 
   if (process.env.XDG_SESSION_TYPE === 'x11') {
     Backend = require('./x11/backend.js').default;
@@ -29,7 +29,7 @@ if (os.platform() === 'linux') {
 
 } else if (os.platform() === 'win32') {
 
-  console.log(`Running on Windows ${os.release()}!`);
+  console.log(`Running on Windows ${os.release()}.`);
   Backend = require('./win32/backend.js').default;
 
 } else if (os.platform() === 'darwin') {
