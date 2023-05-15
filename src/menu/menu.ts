b191fa476eac0398702349a04a2266175dc527e6
Simon Schneegans <code@simonschneegans.de>
2023-05-15 18:24:09 +0200

    :tada: Implement very basic menu interaction

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -15,12 +15,20 @@ import './theme.scss';
 import { computeItemAngles, IVec2 } from './math';
 import { INode } from './node';
 
+enum NodeState {
+  PARENT,
+  ACTIVE,
+  CHILD,
+  GRANDCHILD,
+}
+
 export class Menu {
   private container: HTMLElement = null;
   private root: INode = null;
 
   private hoveredNode: INode = null;
   private draggedNode: INode = null;
+  private selectedNodes: Array<INode> = [];
 
   private menuPosition: IVec2 = { x: 0, y: 0 };
   private mousePosition: IVec2 = { x: 0, y: 0 };
@@ -43,8 +51,10 @@ export class Menu {
       e.stopPropagation();
 
       if (this.root) {
-        this.mousePosition.x = e.clientX - this.menuPosition.x;
-        this.mousePosition.y = e.clientY - this.menuPosition.y;
+        const rect =
+          this.selectedNodes[this.selectedNodes.length - 1].div.getBoundingClientRect();
+        this.mousePosition.x = e.clientX - rect.x;
+        this.mousePosition.y = e.clientY - rect.y;
 
         this.mouseDistance = Math.sqrt(
           this.mousePosition.x * this.mousePosition.x +
@@ -71,7 +81,7 @@ export class Menu {
       this.mouseIsDown = true;
 
       if (this.hoveredNode) {
-        this.setDraggedNode(this.hoveredNode);
+        this.dragNode(this.hoveredNode);
       }
 
       this.redraw();
@@ -83,8 +93,11 @@ export class Menu {
 
       this.mouseIsDown = false;
 
-      this.setDraggedNode(null);
-      this.redraw();
+      if (this.draggedNode) {
+        this.selectNode(this.draggedNode);
+        this.dragNode(null);
+        this.redraw();
+      }
     });
   }
 
@@ -133,8 +146,8 @@ export class Menu {
     this.setupAngles(this.root);
     this.createNodeTree(this.root, this.container);
 
+    this.selectNode(this.root);
     this.redraw();
-    this.root.div.classList.add('active');
 
     this.root.div.style.transform = `translate(${this.menuPosition.x}px, ${this.menuPosition.y}px)`;
   }
@@ -145,6 +158,10 @@ export class Menu {
 
     this.container.innerHTML = '';
     this.root = null;
+
+    this.hoveredNode = null;
+    this.draggedNode = null;
+    this.selectedNodes = [];
   }
 
   // --------------------------------------------------------------------- private methods
@@ -177,14 +194,57 @@ export class Menu {
     }
   }
 
-  private redraw() {
-    this.hoveredNode = null;
-
-    if (this.draggedNode && this.mouseDistance < this.CENTER_RADIUS) {
-      this.setDraggedNode(null);
+  /**
+   * This method computes the node which is currently hovered by the mouse. This is either
+   * one of the children of the currently selected item or the parent of the currently
+   * selected item. The parent will be returned if the mouse pointer is either in the
+   * parent's wedge or in the center of the menu.
+   *
+   * @returns The node that is currently hovered by the mouse. Can be null if the center
+   *   of the root menu is hovered.
+   */
+  private computeHoveredNode(): INode {
+    // If the mouse is in the center of the menu, return the parent of the currently
+    // selected node.
+    if (this.mouseDistance < this.CENTER_RADIUS) {
+      if (this.selectedNodes.length > 1) {
+        return this.selectedNodes[this.selectedNodes.length - 2];
+      }
+      return null;
     }
 
-    this.updateNodeTransform(this.root, 0);
+    // If the mouse is not in the center, check if it is in one of the children of the
+    // currently selected node.
+    for (const child of this.selectedNodes[this.selectedNodes.length - 1].children) {
+      if (
+        (this.mouseAngle > child.startAngle && this.mouseAngle <= child.endAngle) ||
+        (this.mouseAngle - 360 > child.startAngle &&
+          this.mouseAngle - 360 <= child.endAngle)
+      ) {
+        return child;
+      }
+    }
+
+    // If the mouse is not in the center and not in one of the children, it is most likely
+    // in the parent's wedge. Return the parent of the currently selected node.
+    if (this.selectedNodes.length > 1) {
+      return this.selectedNodes[this.selectedNodes.length - 2];
+    }
+
+    // This should actually never happen.
+    return null;
+  }
+
+  private redraw() {
+    this.hoverNode(this.computeHoveredNode());
+
+    if (this.draggedNode && this.draggedNode !== this.hoveredNode) {
+      this.dragNode(this.hoveredNode);
+    }
+
+    if (this.draggedNode && this.mouseDistance < this.CENTER_RADIUS) {
+      this.dragNode(null);
+    }
 
     if (
       this.mouseIsDown &&
@@ -192,50 +252,41 @@ export class Menu {
       this.mouseDistance > this.CENTER_RADIUS &&
       this.hoveredNode
     ) {
-      this.setDraggedNode(this.hoveredNode);
+      this.dragNode(this.hoveredNode);
     }
+
+    const activeNode = this.selectedNodes.length === 1;
+    this.updateNode(this.root, activeNode ? NodeState.ACTIVE : NodeState.PARENT);
   }
 
-  private updateNodeTransform(node: INode, level: number) {
-    if (level > 2) {
-      return;
-    }
-
-    if (level === 0) {
-      if (this.mouseDistance > this.CENTER_RADIUS) {
-        node.div.classList.remove('hovered');
-      } else {
-        node.div.classList.add('hovered');
+  private updateNode(node: INode, state: NodeState) {
+    if (state === NodeState.PARENT) {
+      for (const child of node.children) {
+        const index = this.selectedNodes.indexOf(child);
+        if (index === this.selectedNodes.length - 1) {
+          this.updateNode(child, NodeState.ACTIVE);
+        } else if (index >= 0) {
+          this.updateNode(child, NodeState.PARENT);
+        } else {
+          this.updateNode(child, NodeState.GRANDCHILD);
+        }
       }
-    } else if (level === 1) {
+    } else if (state === NodeState.ACTIVE) {
+      for (const child of node.children) {
+        this.updateNode(child, NodeState.CHILD);
+      }
+    } else if (state === NodeState.CHILD) {
       let transform = '';
-      let hovered = false;
 
       if (this.mouseDistance > this.CENTER_RADIUS) {
-        hovered =
-          (this.mouseAngle > node.startAngle && this.mouseAngle <= node.endAngle) ||
-          (this.mouseAngle - 360 > node.startAngle &&
-            this.mouseAngle - 360 <= node.endAngle);
-
         const angleDiff = Math.abs(node.angle - this.mouseAngle);
         let scale = 1.0 + 0.15 * Math.pow(1 - angleDiff / 180, 4.0);
 
-        if (hovered) {
+        if (node === this.hoveredNode) {
           scale += 0.05;
         }
 
         transform = `scale(${scale}) `;
-      }
-
-      if (hovered) {
-        this.hoveredNode = node;
-        node.div.classList.add('hovered');
-
-        if (this.draggedNode && this.draggedNode !== node) {
-          this.setDraggedNode(node);
-        }
-      } else {
-        node.div.classList.remove('hovered');
       }
 
       if (node === this.draggedNode) {
@@ -247,18 +298,21 @@ export class Menu {
       }
 
       node.div.style.transform = transform;
-    } else if (level === 2) {
+      for (const child of node.children) {
+        this.updateNode(child, NodeState.GRANDCHILD);
+      }
+    } else if (state === NodeState.GRANDCHILD) {
       const x = this.GRANDCHILD_DISTANCE * Math.cos(((node.angle - 90) * Math.PI) / 180);
       const y = this.GRANDCHILD_DISTANCE * Math.sin(((node.angle - 90) * Math.PI) / 180);
       node.div.style.transform = `translate(${x}px, ${y}px)`;
     }
-
-    for (const child of node.children) {
-      this.updateNodeTransform(child, level + 1);
-    }
   }
 
-  private setDraggedNode(node?: INode) {
+  private dragNode(node?: INode) {
+    if (this.draggedNode === node) {
+      return;
+    }
+
     if (this.draggedNode) {
       this.draggedNode.div.classList.remove('dragged');
       this.draggedNode = null;
@@ -268,6 +322,64 @@ export class Menu {
       this.draggedNode = node;
       this.draggedNode.div.classList.add('dragged');
     }
+  }
+
+  private hoverNode(node?: INode) {
+    if (this.hoveredNode === node) {
+      return;
+    }
+
+    if (this.hoveredNode) {
+      this.hoveredNode.div.classList.remove('hovered');
+      this.hoveredNode = null;
+    }
+
+    if (node) {
+      this.hoveredNode = node;
+      this.hoveredNode.div.classList.add('hovered');
+    }
+
+    const activeNode = this.selectedNodes[this.selectedNodes.length - 1];
+    if (node != null && activeNode.children.indexOf(node) >= 0) {
+      activeNode.div.classList.add('child-hovered');
+    } else {
+      activeNode.div.classList.remove('child-hovered');
+    }
+  }
+
+  private selectNode(node: INode) {
+    // If there are no selected nodes, select the given node. This will be the root node.
+    if (this.selectedNodes.length === 0) {
+      node.div.classList.add('active');
+      this.selectedNodes.push(node);
+      return;
+    }
+
+    // If the node is already selected, do nothing.
+    if (this.selectedNodes[this.selectedNodes.length - 1] === node) {
+      return;
+    }
+
+    const previous = this.selectedNodes[this.selectedNodes.length - 1];
+    previous.div.classList.remove('active');
+
+    // If the node is the parent of the currently selected node, we have to pop the
+    // currently selected node from the list of selected nodes.
+    if (this.selectedNodes.length > 1) {
+      const parent = this.selectedNodes[this.selectedNodes.length - 2];
+      if (parent === node) {
+        parent.div.classList.add('active');
+        parent.div.classList.remove('parent');
+        this.selectedNodes.pop();
+        return;
+      }
+    }
+
+    // If the node is a child of the currently selected node, we have to push it to the
+    // list of selected nodes.
+    previous.div.classList.add('parent');
+    node.div.classList.add('active');
+    this.selectedNodes.push(node);
   }
 
   /**
@@ -283,7 +395,8 @@ export class Menu {
       return;
     }
 
-    const angles = computeItemAngles(node.children, (node.angle + 180) % 360);
+    const parentAngle = (node.angle + 180) % 360;
+    const angles = computeItemAngles(node.children, parentAngle);
 
     for (let i = 0; i < node.children.length; ++i) {
       let previousAngle = angles[(i - 1 + angles.length) % angles.length];
@@ -297,6 +410,16 @@ export class Menu {
       if (angles[i] < previousAngle) {
         nextAngle -= 360;
         previousAngle -= 360;
+      }
+
+      if (previousAngle < parentAngle && parentAngle < angles[i]) {
+        previousAngle = parentAngle;
+      } else if (previousAngle < parentAngle + 360 && parentAngle + 360 < angles[i]) {
+        previousAngle = parentAngle + 360;
+      } else if (angles[i] < parentAngle && parentAngle < nextAngle) {
+        nextAngle = parentAngle;
+      } else if (angles[i] < parentAngle + 360 && parentAngle + 360 < nextAngle) {
+        nextAngle = parentAngle + 360;
       }
 
       const child = node.children[i];
