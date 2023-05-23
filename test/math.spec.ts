52d123a6bb965dded5052b9c40b7e298731d4612
Simon Schneegans <code@simonschneegans.de>
2023-05-23 08:54:18 +0200

    :white_check_mark: Add more tests

--- test/math.spec.ts
+++ test/math.spec.ts
@@ -13,27 +13,53 @@ import { computeItemAngles } from '../src/menu/math';
 import { assert } from 'chai';
 
 describe('computeItemAngles', () => {
+  it('should return an empty array for an empty list of items', () => {
+    const items = [];
+    let angles = computeItemAngles(items);
+    assert.deepEqual(angles, []);
+
+    angles = computeItemAngles(items, 90);
+    assert.deepEqual(angles, []);
+  });
+
   it('should return the correct angles for a list of items', () => {
     const items = [{}, {}, {}, {}];
     const angles = computeItemAngles(items);
     assert.deepEqual(angles, [0, 90, 180, 270]);
   });
 
-  it('should return an empty array for an empty list of items', () => {
-    const items = [];
-    const angles = computeItemAngles(items);
-    assert.deepEqual(angles, []);
-  });
-
-  it('should return an empty array if an empty list and a parent angle is given', () => {
-    const items = [];
-    const angles = computeItemAngles(items, 90);
-    assert.deepEqual(angles, []);
-  });
-
   it('should leave some space for a parent item', () => {
-    const items = [{}, {}, {}, {}, {}];
-    const angles = computeItemAngles(items, 25);
-    assert.deepEqual(angles, [325, 85, 145, 205, 265]);
+    const items = [{}, {}, {}, {}, {}, {}, {}];
+    let angles = computeItemAngles(items, 45);
+    assert.deepEqual(angles, [0, 90, 135, 180, 225, 270, 315]);
+
+    angles = computeItemAngles(items, 180);
+    assert.deepEqual(angles, [0, 45, 90, 135, 225, 270, 315]);
+  });
+
+  it('should obey a fixed angles', () => {
+    let items = [{ angle: 120 }, {}, {}, {}];
+    let angles = computeItemAngles(items);
+    assert.deepEqual(angles, [120, 210, 300, 30]);
+
+    items = [{}, {}, { angle: 120 }, {}];
+    angles = computeItemAngles(items);
+    assert.deepEqual(angles, [300, 30, 120, 210]);
+
+    items = [{ angle: 1 }, { angle: 2 }, { angle: 120 }, { angle: 121 }];
+    angles = computeItemAngles(items);
+    assert.deepEqual(angles, [1, 2, 120, 121]);
+  });
+
+  it('should ignore invalid fixed angles', () => {
+    const items = [{ angle: 120 }, { angle: 119 }, {}, {}];
+    const angles = computeItemAngles(items);
+    assert.deepEqual(angles, [120, 210, 300, 30]);
+  });
+
+  it('should properly handle fixed angles and parent items', () => {
+    const items = [{ angle: 120 }, {}, {}, {}];
+    const angles = computeItemAngles(items, 150);
+    assert.deepEqual(angles, [120, 264, 336, 48]);
   });
 });
