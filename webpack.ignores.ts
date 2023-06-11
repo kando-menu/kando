68ec98cf9c740a1ad9c830054b42d2c6132eba09
Simon Schneegans <code@simonschneegans.de>
2023-06-11 05:54:56 +0200

    :green_heart: Ignore native dependencies of other OSs

---
+++ webpack.ignores.ts
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: CC0-1.0
+
+import os from 'os';
+
+export const ignores: RegExp[] = [];
+
+if (os.platform() !== 'win32') {
+  ignores.push(/NativeWin32\.node$/);
+}
+
+if (os.platform() !== 'linux') {
+  ignores.push(/NativeX11\.node$/);
+}
