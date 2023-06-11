59c09b9c3cb879378f5ea55cd1f7047b8b5449b9
Simon Schneegans <code@simonschneegans.de>
2023-06-11 07:56:02 +0200

    :tada: Add a wayland protocol

---
+++ src/backends/linux/kde/wayland/native/move_pointer.cpp
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//   _  _ ____ _  _ ___  ____                                                           //
+//   |_/  |__| |\ | |  \ |  |    This file belongs to Kando, the cross-platform         //
+//   | \_ |  | | \| |__/ |__|    pie menu. Read more on github.com/kando-menu/kando     //
+//                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: MIT
+
+#include "move_pointer.hpp"
+
+#include "fake-input.h"
+
+namespace move_pointer {
+
+void movePointer(int x, int y) {
+
+  org_kde_kwin_fake_input_pointer_motion_absolute(
+      org_kde_kwin_fake_input, wl_fixed_from_int(x), wl_fixed_from_int(y));
+        
+}
+
+Napi::Object movePointerWrapped(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
+  if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber()) {
+    Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
+  }
+
+  Napi::Number x = info[0].As<Napi::Number>();
+  Napi::Number y = info[1].As<Napi::Number>();
+
+  movePointer(x.Int32Value(), y.Int32Value());
+
+  return Napi::Object::New(env);
+}
+
+void init(Napi::Env env, Napi::Object exports) {
+  exports.Set("movePointer",
+              Napi::Function::New(env, movePointerWrapped));
+}
+
+} // namespace move_pointer
---
+++ src/backends/linux/kde/wayland/native/move_pointer.hpp
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//   _  _ ____ _  _ ___  ____                                                           //
+//   |_/  |__| |\ | |  \ |  |    This file belongs to Kando, the cross-platform         //
+//   | \_ |  | | \| |__/ |__|    pie menu. Read more on github.com/kando-menu/kando     //
+//                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: MIT
+
+#include <napi.h>
+
+namespace move_pointer {
+void init(Napi::Env env, Napi::Object exports);
+}
