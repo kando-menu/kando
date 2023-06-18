f25fc8f479f762affe56f5df0695bbddcfdbc0ab
Simon Schneegans <code@simonschneegans.de>
2023-06-18 18:46:37 +0200

    :tada: Add initial key sequence simulation for windows

--- src/backends/win32/backend.ts
+++ src/backends/win32/backend.ts
@@ -9,9 +9,10 @@
 // SPDX-License-Identifier: MIT
 
 import { screen, globalShortcut } from 'electron';
-import { exec } from 'node:child_process';
 import { native } from './native';
 import { Backend, Shortcut } from '../backend';
+import { IKeySequence } from '../../common';
+import { WindowsKeyNames } from './keys';
 
 /**
  * This backend is used on Windows. It uses the native Win32 API to simulate key presses
@@ -67,31 +68,29 @@ export class Win32Backend implements Backend {
   }
 
   /**
-   * This simulates a shortcut using a powershell command.
+   * This simulates a key sequence using the Windows API.
    *
-   * @param shortcut The shortcut to simulate.
+   * @param keys The keys to simulate.
    * @todo: Add information about the string format of the shortcut.
    */
-  public async simulateKeys(shortcut: string) {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        shortcut = this.toPowershellAccelerator(shortcut);
-      } catch (err) {
-        reject(err);
-        return;
+  public async simulateKeys(keys: IKeySequence) {
+    // The native module expects windows virtual key names, so we first need to convert
+    // the given DOM key names. This is done using the table in keys.ts.
+
+    // Collect all required key names and convert them to X11 key names.
+    const keyNames = keys.map((key) => WindowsKeyNames.get(key.name) ?? key.name);
+
+    // Now simulate the key presses.
+    for (let i = 0; i < keys.length; i++) {
+      // Wait a couple of milliseconds if the key has a delay specified.
+      if (keys[i].delay > 0) {
+        await new Promise((resolve) => {
+          setTimeout(resolve, keys[i].delay);
+        });
       }
 
-      exec(
-        `powershell -command "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('${shortcut}')"`,
-        (err: Error) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
-    });
+      native.simulateKey(keyNames[i], keys[i].down);
+    }
   }
 
   /**
--- src/backends/win32/native/move_pointer.cpp
+++ src/backends/win32/native/move_pointer.cpp
@@ -15,28 +15,24 @@
 
 namespace move_pointer {
 
-void movePointer(int dx, int dy) {
-  POINT p;
-  GetCursorPos(&p);
-  SetCursorPos(dx + p.x, dy + p.y);
-}
-
-Napi::Object movePointerWrapped(const Napi::CallbackInfo& info) {
+Napi::Object movePointer(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber()) {
     Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
   }
 
-  Napi::Number dx = info[0].As<Napi::Number>();
-  Napi::Number dy = info[1].As<Napi::Number>();
+  int dx = info[0].As<Napi::Number>().Int32Value();
+  int dy = info[1].As<Napi::Number>().Int32Value();
 
-  movePointer(dx.Int32Value(), dy.Int32Value());
+  POINT p;
+  GetCursorPos(&p);
+  SetCursorPos(dx + p.x, dy + p.y);
 
   return Napi::Object::New(env);
 }
 
 void init(Napi::Env env, Napi::Object exports) {
-  exports.Set("movePointer", Napi::Function::New(env, movePointerWrapped));
+  exports.Set("movePointer", Napi::Function::New(env, movePointer));
 }
 
 } // namespace move_pointer
