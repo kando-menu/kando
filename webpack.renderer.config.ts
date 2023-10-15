ae800d5f0579367ff496d74a2a28160e4dba3fe6
Simon Schneegans <code@simonschneegans.de>
2023-10-15 13:20:41 +0200

    :art: Apply prettier

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,18 +1,14 @@
 // SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
 // SPDX-License-Identifier: CC0-1.0
 
-import type { Configuration } from "webpack";
+import type { Configuration } from 'webpack';
 
-import { rules } from "./webpack.rules";
-import { plugins } from "./webpack.plugins";
+import { rules } from './webpack.rules';
+import { plugins } from './webpack.plugins';
 
 rules.push({
   test: /\.s[ac]ss$/i,
-  use: [
-    { loader: "style-loader" },
-    { loader: "css-loader" },
-    { loader: "sass-loader" },
-  ],
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
 });
 
 export const rendererConfig: Configuration = {
@@ -21,6 +17,6 @@ export const rendererConfig: Configuration = {
   },
   plugins,
   resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".scss"],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss'],
   },
 };
