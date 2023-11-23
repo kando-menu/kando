cc7afe7b4eaa52b398d07bc59a3d07e35d676002
Simon Schneegans <code@simonschneegans.de>
2023-11-23 08:13:05 +0100

    :tada: Add placeholder texts for the menu preview and properties

--- src/renderer/editor/preview/preview.ts
+++ src/renderer/editor/preview/preview.ts
@@ -8,22 +8,26 @@
 // SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
 // SPDX-License-Identifier: MIT
 
-import { IEditorNode } from './../editor-node';
+import Handlebars from 'handlebars';
 
 export class Preview {
   // The container is the HTML element which contains the menu preview. It is created in
   // the constructor and returned by the getContainer() method.
   private container: HTMLElement = null;
 
-  private root: IEditorNode = null;
-
   /**
    * This constructor creates the HTML elements for the menu preview and wires up all the
    * functionality.
    */
   constructor() {
-    this.container = document.createElement('div');
-    this.container.id = 'kando-menu-preview';
+    const template = Handlebars.compile(require('./templates/preview.hbs').default);
+
+    const div = document.createElement('div');
+    div.innerHTML = template({
+      id: 'kando-menu-preview',
+    });
+
+    this.container = div.firstElementChild as HTMLElement;
   }
 
   /** This method returns the container of the menu preview. */
@@ -31,7 +35,11 @@ export class Preview {
     return this.container;
   }
 
-  public setMenu(root: IEditorNode) {
-    this.root = root;
+  public show() {
+    this.container.classList.add('visible');
+  }
+
+  public hide() {
+    this.container.classList.remove('visible');
   }
 }
--- src/renderer/editor/properties/properties.ts
+++ src/renderer/editor/properties/properties.ts
@@ -8,6 +8,8 @@
 // SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
 // SPDX-License-Identifier: MIT
 
+import Handlebars from 'handlebars';
+
 export class Properties {
   // The container is the HTML element which contains the currently edited menu's
   // properties. It is created in the constructor and returned by the getContainer()
@@ -19,12 +21,26 @@ export class Properties {
    * all the functionality.
    */
   constructor() {
-    this.container = document.createElement('div');
-    this.container.id = 'kando-menu-properties';
+    const template = Handlebars.compile(require('./templates/properties.hbs').default);
+
+    const div = document.createElement('div');
+    div.innerHTML = template({
+      id: 'kando-menu-properties',
+    });
+
+    this.container = div.firstElementChild as HTMLElement;
   }
 
   /** This method returns the container of the menu preview. */
   public getContainer(): HTMLElement {
     return this.container;
+  }
+
+  public show() {
+    this.container.classList.add('visible');
+  }
+
+  public hide() {
+    this.container.classList.remove('visible');
   }
 }
