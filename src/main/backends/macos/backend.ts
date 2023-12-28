fec63e74306c6c4e6184079a7675ed975660281e
Simon Schneegans <code@simonschneegans.de>
2023-12-28 21:57:25 +0100

    :tada: Add initial macOS backend

---
+++ src/main/backends/macos/backend.ts
@@ -0,0 +1,97 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//   _  _ ____ _  _ ___  ____                                                           //
+//   |_/  |__| |\ | |  \ |  |    This file belongs to Kando, the cross-platform         //
+//   | \_ |  | | \| |__/ |__|    pie menu. Read more on github.com/kando-menu/kando     //
+//                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: MIT
+
+import { screen, globalShortcut, app } from 'electron';
+import { Backend, Shortcut } from '../backend';
+import { IKeySequence } from '../../../common';
+import { MacosKeyNames } from './keys';
+
+export class MacosBackend implements Backend {
+  /**
+   * On macOS, the window type is set to 'panel'. This makes sure that the window is
+   * always on top of other windows and that it is shown on all workspaces.
+   *
+   * @returns 'panel'
+   */
+  public getWindowType() {
+    return 'panel';
+  }
+
+  /** On macOS, we use this to hide the dock icon. */
+  public async init() {
+    // Is there a way to hide the dock icon on macOS initially? If we hide it here, it
+    // will be shown for a short moment when the app is started.
+    app.dock.hide();
+  }
+
+  /**
+   * @returns The name and class of the currently focused window as well as the current
+   *   pointer position.
+   */
+  public async getWMInfo() {
+    const pointer = screen.getCursorScreenPoint();
+
+    // For some reason, this makes the method much faster. For now, I have no idea why.
+    process.nextTick(() => {});
+
+    return {
+      windowName: 'foo',
+      windowClass: 'bar',
+      pointerX: pointer.x,
+      pointerY: pointer.y,
+    };
+  }
+
+  /**
+   * Moves the pointer by the given amount.
+   *
+   * @param dx The amount of horizontal movement.
+   * @param dy The amount of vertical movement.
+   */
+  public async movePointer(dx: number, dy: number) {
+    // TODO: Implement this.
+  }
+
+  /**
+   * If one of the given keys in the sequence is not known, an exception will be thrown.
+   *
+   * @param shortcut The keys to simulate.
+   */
+  public async simulateKeys(keys: IKeySequence) {
+    // TODO: Implement this.
+  }
+
+  /**
+   * This binds a shortcut. The action callback of the shortcut is called when the
+   * shortcut is pressed. On macOS, this uses Electron's globalShortcut module.
+   *
+   * @param shortcut The shortcut to bind.
+   * @returns A promise which resolves when the shortcut has been bound.
+   */
+  public async bindShortcut(shortcut: Shortcut) {
+    if (!globalShortcut.register(shortcut.accelerator, shortcut.action)) {
+      throw new Error('Shortcut is already in use.');
+    }
+  }
+
+  /**
+   * This unbinds a previously bound shortcut.
+   *
+   * @param shortcut The shortcut to unbind.
+   */
+  public async unbindShortcut(shortcut: Shortcut) {
+    globalShortcut.unregister(shortcut.accelerator);
+  }
+
+  /** This unbinds all previously bound shortcuts. */
+  public async unbindAllShortcuts() {
+    globalShortcut.unregisterAll();
+  }
+}
