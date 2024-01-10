f15d29807b0ee6b4c82de61f193f768a18167f70
Simon Schneegans <code@simonschneegans.de>
2024-01-10 21:22:53 +0100

    :wrench: Use a capital K for the installer app name

--- forge.config.ts
+++ forge.config.ts
@@ -31,7 +31,6 @@ const config: ForgeConfig = {
     new MakerRpm({
       // https://js.electronforge.io/interfaces/_electron_forge_maker_rpm.InternalOptions.MakerRpmConfigOptions.html
       options: {
-        productName: 'Kando',
         genericName: 'Pie Menu',
         icon: 'assets/icons/icon.svg',
         homepage: 'https://github.com/kando-menu/kando',
@@ -42,7 +41,6 @@ const config: ForgeConfig = {
     new MakerDeb({
       // https://js.electronforge.io/interfaces/_electron_forge_maker_deb.InternalOptions.MakerDebConfigOptions.html
       options: {
-        productName: 'Kando',
         genericName: 'Pie Menu',
         icon: 'assets/icons/icon.svg',
         homepage: 'https://github.com/kando-menu/kando',
