389a4339b23884d8a3653431437adcd92799ca91
Simon Schneegans <code@simonschneegans.de>
2024-03-24 14:53:15 +0100

    :recycle: Move menus tab logic into its own source file

---
+++ src/renderer/editor/toolbar/menus-tab.ts
@@ -0,0 +1,154 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//   _  _ ____ _  _ ___  ____                                                           //
+//   |_/  |__| |\ | |  \ |  |    This file belongs to Kando, the cross-platform         //
+//   | \_ |  | | \| |__/ |__|    pie menu. Read more on github.com/menu/kando           //
+//                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: MIT
+
+import Handlebars from 'handlebars';
+import { ItemDragger } from '../common/item-dragger';
+import { EventEmitter } from 'events';
+import { IMenu } from '../../../common';
+import * as themedIcon from '../common/themed-icon';
+
+/**
+ * This class is responsible for the menus tab in the toolbar. It is an event emitter
+ * which emits the following events:
+ *
+ * - 'select-menu': This event is emitted when the user selects a menu in the toolbar. The
+ *   index of the selected menu is passed as the first argument.
+ * - 'add-menu': This event is emitted when the user clicks the "Add Menu" button.
+ * - 'delete-menu': This event is emitted when the user drags a menu to the trash tab.
+ */
+export class MenusTab extends EventEmitter {
+  /** The container is the HTML element which contains the entire toolbar. */
+  private container: HTMLElement = null;
+
+  /** This is the HTML element which contains the menus tab's content. */
+  private tab: HTMLElement = null;
+
+  /** This is used to drag'n'drop menus from the toolbar to the trash. */
+  private menuDragger = new ItemDragger();
+
+  /**
+   * This constructor is called after the general toolbar DOM has been created.
+   *
+   * @param container The container is the HTML element which contains the entire toolbar.
+   */
+  constructor(container: HTMLElement) {
+    super();
+
+    // Store a reference to the container. We will attach menu buttons divs to it during
+    // drag'n'drop operations.
+    this.container = container;
+
+    // The tab has been created in the toolbar's constructor.
+    this.tab = this.container.querySelector('#kando-menus-tab');
+
+    // If a menu button is clicked, we emit the 'select-menu' event. If the "Add Menu"
+    // button is clicked, we emit the 'add-menu' event.
+    this.tab.addEventListener('click', (event) => {
+      const input = event.target as HTMLInputElement;
+      if (input && input.name === 'menu-selection-button') {
+        this.emit('select-menu', input.dataset.index);
+      }
+
+      const button = event.target as HTMLButtonElement;
+      if (button && button.classList.contains('add-menu-button')) {
+        this.emit('add-menu');
+      }
+    });
+
+    // During drag'n'drop operations, we need to append the dragged div to the outer
+    // container to be able to drag it outside of the scrollable area. Here we set up
+    // the drag'n'drop logic.
+
+    let originalParent: HTMLElement;
+
+    this.menuDragger.on('drag-start', (index, div) => {
+      // Add a class to the toolbar area to indicate that we are dragging a deletable
+      // item. This will make the trash tab more visible.
+      document
+        .getElementById('kando-editor-toolbar-area')
+        .classList.add('dragging-deletable-item');
+
+      // Set fixed width and height for dragged item.
+      const rect = div.getBoundingClientRect();
+      div.style.width = `${rect.width}px`;
+      div.style.height = `${rect.height}px`;
+
+      // Remember the original parent so that we can reinsert the div later.
+      originalParent = div.parentNode;
+
+      // Append the div to the outer container. This is necessary because the div is
+      // inside a scrollable container and we want to be able to drag it outside.
+      div.classList.add('dragging');
+      this.container.appendChild(div);
+    });
+
+    this.menuDragger.on(
+      'drag-move',
+      (index, div, relative, absolute, offset, grabOffset) => {
+        div.style.transform = `translate(${absolute.x - grabOffset.x}px, ${absolute.y - grabOffset.y}px)`;
+      }
+    );
+
+    this.menuDragger.on('drag-end', (index, div) => {
+      div.classList.remove('dragging');
+      div.style.transform = '';
+
+      // Clear the fixed width and height.
+      div.style.width = '';
+      div.style.height = '';
+
+      // Reinsert the div back to its original position.
+      originalParent.appendChild(div);
+
+      document
+        .getElementById('kando-editor-toolbar-area')
+        .classList.remove('dragging-deletable-item');
+
+      // Check if the trash tab is hovered.
+      const tab = this.container.querySelector(
+        ".nav-link[data-bs-target='#kando-trash-tab']"
+      );
+
+      if (tab.matches(':hover')) {
+        this.emit('delete-menu', index);
+      }
+    });
+  }
+
+  /**
+   * This method is called by the toolbar whenever the list of menus changes. It
+   * completely rebuilds the menus tab.
+   *
+   * @param menus A list of menus.
+   * @param currentMenu The index of the currently selected menu.
+   */
+  public setMenus(menus: Array<IMenu>, currentMenu: number) {
+    this.menuDragger.removeAllDraggables();
+
+    const template = Handlebars.compile(require('./templates/menus-tab.hbs').default);
+
+    // Compile the data for the Handlebars template.
+    const data = menus.map((menu, index) => ({
+      name: menu.nodes.name,
+      active: index === currentMenu,
+      shortcut: menu.shortcut || 'Not bound',
+      icon: themedIcon.createDiv(menu.nodes.icon, menu.nodes.iconTheme).outerHTML,
+      index,
+    }));
+
+    this.tab.innerHTML = template({ menus: data });
+
+    // Add drag'n'drop logic to the menu buttons.
+    for (const menu of data) {
+      const div = document.getElementById(`menu-button-${menu.index}`);
+      this.menuDragger.addDraggable(div, menu.index);
+    }
+  }
+}
--- src/renderer/editor/toolbar/toolbar.ts
+++ src/renderer/editor/toolbar/toolbar.ts
@@ -11,8 +11,7 @@
 import Handlebars from 'handlebars';
 import { EventEmitter } from 'events';
 import { IMenu } from '../../../common';
-import { ItemDragger } from '../common/item-dragger';
-import * as themedIcon from '../common/themed-icon';
+import { MenusTab } from './menus-tab';
 
 /**
  * This class is responsible for the toolbar on the bottom of the editor screen. It is an
@@ -20,14 +19,17 @@ import * as themedIcon from '../common/themed-icon';
  *
  * - 'enter-edit-mode': This event is emitted when the user enters edit mode.
  * - 'leave-edit-mode': This event is emitted when the user leaves edit mode.
- * - 'select-menu': This event is emitted when the user selects a menu in the toolbar. The
- *   index of the selected menu is passed as the first argument.
- * - 'add-menu': This event is emitted when the user clicks the "Add Menu" button.
- * - 'delete-menu': This event is emitted when the user drags a menu to the trash tab.
  * - 'expand': This event is emitted when a tab is selected which should cover the entire
  *   editor.
  * - 'collapse': This event is emitted when a tab is selected which should not cover the
  *   entire editor.
+ *
+ * The following events are forwarded from the menus tab:
+ *
+ * - 'select-menu': This event is emitted when the user selects a menu in the toolbar. The
+ *   index of the selected menu is passed as the first argument.
+ * - 'add-menu': This event is emitted when the user clicks the "Add Menu" button.
+ * - 'delete-menu': This event is emitted when the user drags a menu to the trash tab.
  */
 export class Toolbar extends EventEmitter {
   /**
@@ -36,8 +38,8 @@ export class Toolbar extends EventEmitter {
    */
   private container: HTMLElement = null;
 
-  /** This is used to drag'n'drop menus from the toolbar. */
-  private menuDragger = new ItemDragger();
+  /** This manages the first tab of the toolbar. */
+  private menusTab: MenusTab = null;
 
   /**
    * This constructor creates the HTML elements for the toolbar and wires up all the
@@ -49,6 +51,12 @@ export class Toolbar extends EventEmitter {
     this.loadContent();
     this.initVisibility();
     this.initTabs();
+
+    // Initialize the menus tab and forward its events.
+    this.menusTab = new MenusTab(this.container);
+    this.menusTab.on('add-menu', () => this.emit('add-menu'));
+    this.menusTab.on('select-menu', (index) => this.emit('select-menu', index));
+    this.menusTab.on('delete-menu', (index) => this.emit('delete-menu', index));
   }
 
   /** This method returns the container of the editor toolbar. */
@@ -56,26 +64,14 @@ export class Toolbar extends EventEmitter {
     return this.container;
   }
 
+  /**
+   * Whenever a menu is added or removed, the menus tab needs to be updated.
+   *
+   * @param menus A list of all menus.
+   * @param currentMenu The index of the currently selected menu.
+   */
   public setMenus(menus: Array<IMenu>, currentMenu: number) {
-    this.menuDragger.removeAllDraggables();
-
-    const template = Handlebars.compile(require('./templates/menus-tab.hbs').default);
-    const menusTab = this.container.querySelector('#kando-menus-tab');
-
-    const data = menus.map((menu, index) => ({
-      name: menu.nodes.name,
-      active: index === currentMenu,
-      shortcut: menu.shortcut || 'Not bound',
-      icon: themedIcon.createDiv(menu.nodes.icon, menu.nodes.iconTheme).outerHTML,
-      index,
-    }));
-
-    menusTab.innerHTML = template({ menus: data });
-
-    for (const menu of data) {
-      const div = document.getElementById(`menu-button-${menu.index}`);
-      this.menuDragger.addDraggable(div, menu.index);
-    }
+    this.menusTab.setMenus(menus, currentMenu);
   }
 
   /** This method loads the HTML content of the toolbar. */
@@ -145,71 +141,6 @@ export class Toolbar extends EventEmitter {
           }),
         },
       ],
-    });
-
-    const menusTab = this.container.querySelector('#kando-menus-tab');
-    menusTab.addEventListener('click', (event) => {
-      const input = event.target as HTMLInputElement;
-      if (input && input.name === 'menu-selection-button') {
-        this.emit('select-menu', input.dataset.index);
-      }
-
-      const button = event.target as HTMLButtonElement;
-      if (button && button.classList.contains('add-menu-button')) {
-        this.emit('add-menu');
-      }
-    });
-
-    let originalParent: HTMLElement;
-
-    this.menuDragger.on('drag-start', (index, div) => {
-      document
-        .getElementById('kando-editor-toolbar-area')
-        .classList.add('dragging-deletable-item');
-
-      // Set fixed width and height for dragged item.
-      const rect = div.getBoundingClientRect();
-      div.style.width = `${rect.width}px`;
-      div.style.height = `${rect.height}px`;
-
-      // Remember the original parent and the next sibling of the div
-      originalParent = div.parentNode;
-
-      // Append the div to the body.
-      div.classList.add('dragging');
-      this.container.appendChild(div);
-    });
-
-    this.menuDragger.on(
-      'drag-move',
-      (index, div, relative, absolute, offset, grabOffset) => {
-        div.style.transform = `translate(${absolute.x - grabOffset.x}px, ${absolute.y - grabOffset.y}px)`;
-      }
-    );
-
-    this.menuDragger.on('drag-end', (index, div) => {
-      div.classList.remove('dragging');
-      div.style.transform = '';
-
-      // Clear the fixed width and height.
-      div.style.width = '';
-      div.style.height = '';
-
-      // Reinsert the div back to its original position.
-      originalParent.appendChild(div);
-
-      document
-        .getElementById('kando-editor-toolbar-area')
-        .classList.remove('dragging-deletable-item');
-
-      // Check if the trash tab is hovered.
-      const tab = this.container.querySelector(
-        ".nav-link[data-bs-target='#kando-trash-tab']"
-      );
-
-      if (tab.matches(':hover')) {
-        this.emit('delete-menu', index);
-      }
     });
   }
 
