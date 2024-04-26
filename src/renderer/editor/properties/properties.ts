fa8ccb058a1bf8134d212970d8508b31ca261914
Simon Schneegans <code@simonschneegans.de>
2024-04-26 07:25:20 +0200

    :tada: Add some initial menu properties code

--- src/renderer/editor/properties/properties.ts
+++ src/renderer/editor/properties/properties.ts
@@ -8,16 +8,34 @@
 // SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
 // SPDX-License-Identifier: MIT
 
+import { EventEmitter } from 'events';
 import Handlebars from 'handlebars';
-import { IEditorMenuItem } from '../common/editor-menu-item';
 
-export class Properties {
+import { IEditorMenuItem } from '../common/editor-menu-item';
+import { IMenu } from '../../../common';
+import * as themedIcon from '../common/themed-icon';
+import { IconThemeRegistry } from '../../../common/icon-theme-registry';
+
+/**
+ * This class is responsible for displaying the properties of the currently edited menu
+ * item. It emits events when the user changes one of the properties of the currently
+ * edited menu item. The new values are stored in the menu item object before the
+ * corresponding event is emitted, so you can access the new values by accessing the
+ * properties of the menu item object. It is an event emitter which emits the following
+ * events:
+ *
+ * @fires changed-name - When the user changed the name of the current menu item.
+ * @fires changed-icon - When the user changed the icon of the current menu item.
+ * @fires changed-icon-theme - When the user changed the theme of the current menu item.
+ */
+export class Properties extends EventEmitter {
   // The container is the HTML element which contains the currently edited menu's
   // properties. It is created in the constructor and returned by the getContainer()
   // method.
   private container: HTMLElement = null;
 
   private nameInput: HTMLInputElement = null;
+  private iconButton: HTMLButtonElement = null;
 
   private activeItem: IEditorMenuItem = null;
 
@@ -26,6 +44,8 @@ export class Properties {
    * all the functionality.
    */
   constructor() {
+    super();
+
     const template = Handlebars.compile(require('./templates/properties.hbs').default);
 
     const div = document.createElement('div');
@@ -37,10 +57,20 @@ export class Properties {
     this.nameInput.addEventListener('input', () => {
       if (this.activeItem) {
         this.activeItem.name = this.nameInput.value;
+        this.emit('changed-name');
       }
     });
 
+    this.iconButton = div.querySelector(
+      '#kando-menu-properties-icon-button'
+    ) as HTMLButtonElement;
+
     this.container = div.firstElementChild as HTMLElement;
+
+    const themes = IconThemeRegistry.getInstance().getIconThemes();
+    themes.forEach((theme) => {
+      console.log(theme.name);
+    });
   }
 
   /** This method returns the container of the menu preview. */
@@ -56,9 +86,13 @@ export class Properties {
     this.container.classList.remove('visible');
   }
 
+  public setMenu(menu: IMenu) {
+    this.setItem(menu.nodes);
+  }
+
   public setItem(item: IEditorMenuItem) {
     this.activeItem = item;
-
     this.nameInput.value = item.name;
+    this.iconButton.innerHTML = themedIcon.createDiv(item.icon, item.iconTheme).outerHTML;
   }
 }
