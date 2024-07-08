1ecb862b14e2ea906108507854bec549de98898c
Simon Schneegans <code@simonschneegans.de>
2024-07-08 05:47:31 +0200

    :sparkles: Apply prettier

--- src/main/app.ts
+++ src/main/app.ts
@@ -377,13 +377,15 @@ export class KandoApp {
           width: workarea.width + 1,
           height: workarea.height + 1,
         });
-        
+
         // On MacOS we need to ensure the window is on the current workspace before showing.
         // This is the fix to issue #461: https://github.com/kando-menu/kando/issues/461
         if (process.platform === 'darwin') {
           this.window.setVisibleOnAllWorkspaces(true, { skipTransformProcessType: true });
           setTimeout(() => {
-          this.window.setVisibleOnAllWorkspaces(false, { skipTransformProcessType: true });
+            this.window.setVisibleOnAllWorkspaces(false, {
+              skipTransformProcessType: true,
+            });
           }, 100);
         }
 
@@ -500,9 +502,9 @@ export class KandoApp {
     // We set the Activation Policy to Accessory so that Kando doesn't show in dock
     // or the CMD Tab App Switcher. This is only for MacOS.
     if (process.platform === 'darwin') {
-    app.setActivationPolicy('accessory')
+      app.setActivationPolicy('accessory');
     }
-    
+
     // If the user clicks on a link, we close Kando's window and open the link in the
     // default browser.
     this.window.webContents.setWindowOpenHandler(({ url }) => {
