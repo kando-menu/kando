93717349330ee1d13137e768967405a383e24ca6
Simon Schneegans <code@simonschneegans.de>
2024-07-16 19:54:18 +0200

    :tada: Add app-image creation script

---
+++ tools/make-app-image.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+# SPDX-License-Identifier: CC0-1.0
+
+# This script assumes that you have bundled Kando with `npm run package` before. It will
+# create a temporary build/kando.AppDir and copy the necessary files into it. Then it will
+# download the appimagetool and create an AppImage in out/make/appimage.
+
+# Exit on errors.
+set -e
+set -u
+
+# Get the directory of this script.
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+INPUT_DIR="$SCRIPT_DIR/../out/Kando-linux-x64"
+BUILD_DIR="$SCRIPT_DIR/../build/kando.AppDir"
+OUTPUT_DIR="$SCRIPT_DIR/../out/make/appimage"
+
+# Clean the build directory.
+mkdir -p "$OUTPUT_DIR"
+rm -r "$BUILD_DIR" || true
+mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR"
+
+VERSION=$(grep '"version":' ../../package.json | awk -F '"' '{print $4}')
+ARCH=x86_64
+echo "Creating AppImage for Kando $VERSION..."
+
+# Copy desktop file, icon, and all the other stuff.
+cp -a "$INPUT_DIR"/* .
+cp "$SCRIPT_DIR/kando.desktop" .
+cp resources/app/.webpack/main/assets/icon*png ./kando.png && ln -s ./kando.png ./.DirIcon
+
+# Create the AppRun file.
+cat >> ./AppRun << 'EOF'
+#!/bin/sh
+CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+exec "$CURRENTDIR"/kando "$@"
+EOF
+chmod a+x ./AppRun
+
+# Get the appimagetool.
+cd ..
+APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*86_64.*mage$')
+echo "Downloading appimagetool from $APPIMAGETOOL..."
+wget -q "$APPIMAGETOOL" -O ./appimagetool
+chmod a+x ./appimagetool
+
+# Do the thing!
+export ARCH
+export VERSION
+./appimagetool -s "$BUILD_DIR"
+
+# Move the AppImage to the output directory.
+mv "Kando-$VERSION-$ARCH.AppImage" "$OUTPUT_DIR"
+
+echo "All Done!"
