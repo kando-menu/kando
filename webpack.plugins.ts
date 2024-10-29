a455b7d4baa6d7b8f77e8eb02382405f84ee2bff
Simon Schneegans <code@simonschneegans.de>
2024-10-29 13:36:22 +0100

    :fire: Remove ForkTsCheckerWebpackPlugin

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -2,18 +2,11 @@
 // SPDX-License-Identifier: CC0-1.0
 
 import os from 'os';
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { NormalModuleReplacementPlugin, DefinePlugin } from 'webpack';
 import CopyPlugin from 'copy-webpack-plugin';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const plugins: any[] = [
-  new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
-  }),
   // We do not need the otf, ttf, eot and woff files of the SimpleIcons package. They are
   // replaced by the woff2 file below. Maybe there is a more elegant way to do this, but
   // this works for now.
