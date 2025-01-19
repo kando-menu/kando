a2e301d93bece2cd9594da08d3267795db43c92b
Simon Schneegans <code@simonschneegans.de>
2025-01-19 17:13:43 +0100

    :wrench: Add test layout

--- src/settings-renderer/components/App.tsx
+++ src/settings-renderer/components/App.tsx
@@ -1,16 +1,26 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//   _  _ ____ _  _ ___  ____                                                           //
+//   |_/  |__| |\ | |  \ |  |    This file belongs to Kando, the cross-platform         //
+//   | \_ |  | | \| |__/ |__|    pie menu. Read more on github.com/kando-menu/kando     //
+//                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: MIT
+
 import React from 'react'
 
 import * as classes from './App.module.scss'
 
-function app() {
+function component() {
 
   return (
     <div className={classes.container}>
-      <h1>Hi, Electron!</h1>
-
-      <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+      <div className={classes.sidebar} />
+      <div className={classes.preview} />
+      <div className={classes.sidebar} />
     </div>
   )
 }
 
-export default app
+export default component
