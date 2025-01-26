d97b9504f3eb873e329878b9610c06a6cc923530
Simon Schneegans <code@simonschneegans.de>
2025-01-26 07:42:29 +0100

    :wrench: Add sclae-spin button

--- src/settings-renderer/components/MenuThemesDialog.tsx
+++ src/settings-renderer/components/MenuThemesDialog.tsx
@@ -13,6 +13,7 @@ import i18next from 'i18next';
 
 import Modal from './widgets/Modal';
 import ManagedCheckbox from './widgets/ManagedCheckbox';
+import ManagedSpinbutton from './widgets/ManagedSpinbutton';
 
 interface IProps {
   visible: boolean;
@@ -30,6 +31,14 @@ export default (props: IProps) => {
         label={i18next.t('toolbar.menu-themes-tab.dark-mode')}
         info={i18next.t('toolbar.menu-themes-tab.system-mode-subheading')}
         settingsKey="enableDarkModeForMenuThemes"
+      />
+      <ManagedSpinbutton
+        label="Menu Scale"
+        info="The scale of the menu. Default is 1."
+        settingsKey="zoomFactor"
+        min={0.5}
+        max={5}
+        step={0.1}
       />
     </Modal>
   );
