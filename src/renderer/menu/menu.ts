337f534365015bd4ff4480f0c8017fa3fa8648aa
Jon Lake <jonathanjlake@gmail.com>
2025-03-28 15:06:50 -0600

    :tada: Add setting to enable/disable gamepad input (#754).

--- src/renderer/menu/menu.ts
+++ src/renderer/menu/menu.ts
@@ -404,6 +404,8 @@ export class Menu extends EventEmitter {
     };
 
     this.pointerInput.onCloseMenu(onCloseMenu);
+    this.gamepadInput.onCloseMenu(onCloseMenu);
+
     this.pointerInput.onUpdateState(onUpdateState);
     this.gamepadInput.onUpdateState(onUpdateState);
 
