38d52f606fcdf737c0cae92d048317fde093ae2a
Simon Schneegans <code@simonschneegans.de>
2025-03-30 14:32:03 +0200

    :recycle: Move header toolbar to its own component

--- src/settings-renderer/components/Preview.tsx
+++ src/settings-renderer/components/Preview.tsx
@@ -9,85 +9,19 @@
 // SPDX-License-Identifier: MIT
 
 import React from 'react';
-import {
-  TbSettingsFilled,
-  TbInfoSquareRoundedFilled,
-  TbPaletteFilled,
-} from 'react-icons/tb';
-import { IoArrowUndo, IoArrowRedo } from 'react-icons/io5';
 
 import * as classes from './Preview.module.scss';
 
-import { useAppState, useMenuSettings } from '../state';
 import { ItemTypeRegistry } from '../../common/item-type-registry';
-import Headerbar from './widgets/Headerbar';
-import Button from './widgets/Button';
+import PreviewHeaderbar from './PreviewHeaderbar';
 import ThemedIcon from './widgets/ThemedIcon';
 
 export default () => {
-  // This will force a re-render whenever the menu settings change. For now, this is
-  // necessary to update the undo/redo buttons. In the future, we might want to make
-  // this more fine-grained, maybe by directly subscribing to the past and future states
-  // of the temporal state.
-  useMenuSettings();
-
-  const setAboutDialogVisible = useAppState((state) => state.setAboutDialogVisible);
-  const setThemesDialogVisible = useAppState((state) => state.setThemesDialogVisible);
-  const setSettingsDialogVisible = useAppState((state) => state.setSettingsDialogVisible);
-
-  const { futureStates, pastStates } = useMenuSettings.temporal.getState();
-
-  const headerButtons = (
-    <>
-      <span>
-        <Button
-          tooltip="About Kando"
-          icon={<TbInfoSquareRoundedFilled />}
-          onClick={() => setAboutDialogVisible(true)}
-          variant="tool"
-          grouped
-        />
-        <Button
-          tooltip="Menu Themes"
-          icon={<TbPaletteFilled />}
-          onClick={() => setThemesDialogVisible(true)}
-          variant="tool"
-          grouped
-        />
-        <Button
-          tooltip="General Settings"
-          icon={<TbSettingsFilled />}
-          onClick={() => setSettingsDialogVisible(true)}
-          variant="tool"
-          grouped
-        />
-      </span>
-      <span>
-        <Button
-          tooltip="Undo"
-          icon={<IoArrowUndo />}
-          disabled={pastStates.length === 0}
-          onClick={() => useMenuSettings.temporal.getState().undo()}
-          variant="tool"
-          grouped
-        />
-        <Button
-          tooltip="Redo"
-          icon={<IoArrowRedo />}
-          disabled={futureStates.length === 0}
-          onClick={() => useMenuSettings.temporal.getState().redo()}
-          variant="tool"
-          grouped
-        />
-      </span>
-    </>
-  );
-
   const itemTypes = Array.from(ItemTypeRegistry.getInstance().getAllTypes());
 
   return (
     <div className={classes.container}>
-      <Headerbar center={headerButtons} />
+      <PreviewHeaderbar />
       <div className={classes.previewArea}>
         <div className={classes.preview}></div>
       </div>
---
+++ src/settings-renderer/components/PreviewHeaderbar.tsx
@@ -0,0 +1,82 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//   _  _ ____ _  _ ___  ____                                                           //
+//   |_/  |__| |\ | |  \ |  |    This file belongs to Kando, the cross-platform         //
+//   | \_ |  | | \| |__/ |__|    pie menu. Read more on github.com/kando-menu/kando     //
+//                                                                                      //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// SPDX-FileCopyrightText: Simon Schneegans <code@simonschneegans.de>
+// SPDX-License-Identifier: MIT
+
+import React from 'react';
+import {
+  TbSettingsFilled,
+  TbInfoSquareRoundedFilled,
+  TbPaletteFilled,
+} from 'react-icons/tb';
+import { IoArrowUndo, IoArrowRedo } from 'react-icons/io5';
+
+import { useAppState, useMenuSettings } from '../state';
+import Headerbar from './widgets/Headerbar';
+import Button from './widgets/Button';
+
+export default () => {
+  // This will force a re-render whenever the menu settings change. For now, this is
+  // necessary to update the undo/redo buttons. In the future, we might want to make
+  // this more fine-grained, maybe by directly subscribing to the past and future states
+  // of the temporal state.
+  useMenuSettings();
+
+  const setAboutDialogVisible = useAppState((state) => state.setAboutDialogVisible);
+  const setThemesDialogVisible = useAppState((state) => state.setThemesDialogVisible);
+  const setSettingsDialogVisible = useAppState((state) => state.setSettingsDialogVisible);
+
+  const { futureStates, pastStates } = useMenuSettings.temporal.getState();
+
+  const headerButtons = (
+    <>
+      <span>
+        <Button
+          tooltip="About Kando"
+          icon={<TbInfoSquareRoundedFilled />}
+          onClick={() => setAboutDialogVisible(true)}
+          variant="tool"
+          grouped
+        />
+        <Button
+          tooltip="Menu Themes"
+          icon={<TbPaletteFilled />}
+          onClick={() => setThemesDialogVisible(true)}
+          variant="tool"
+          grouped
+        />
+        <Button
+          tooltip="General Settings"
+          icon={<TbSettingsFilled />}
+          onClick={() => setSettingsDialogVisible(true)}
+          variant="tool"
+          grouped
+        />
+      </span>
+      <span>
+        <Button
+          tooltip="Undo"
+          icon={<IoArrowUndo />}
+          disabled={pastStates.length === 0}
+          onClick={() => useMenuSettings.temporal.getState().undo()}
+          variant="tool"
+          grouped
+        />
+        <Button
+          tooltip="Redo"
+          icon={<IoArrowRedo />}
+          disabled={futureStates.length === 0}
+          onClick={() => useMenuSettings.temporal.getState().redo()}
+          variant="tool"
+          grouped
+        />
+      </span>
+    </>
+  );
+  return <Headerbar center={headerButtons} />;
+};
