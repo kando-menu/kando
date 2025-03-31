b397b7c81d9ce8ee4601c93ee62d881112089a12
Simon Schneegans <code@simonschneegans.de>
2025-03-31 18:41:37 +0200

    :wrench: Close menu-item tooltips on escape

--- src/settings-renderer/components/App.tsx
+++ src/settings-renderer/components/App.tsx
@@ -69,6 +69,7 @@ export default () => {
         delayShow={0}
         openEvents={{ click: true }}
         closeEvents={{ click: true }}
+        globalCloseEvents={{ escape: true }}
       />
     </>
   );
