59bb116ba81372e1946de761bb1002700203d84c
Simon Schneegans <code@simonschneegans.de>
2025-06-14 16:39:26 +0200

    :globe_with_meridians: Make new option translatable

--- src/settings-renderer/components/dialogs/GeneralSettingsDialog.tsx
+++ src/settings-renderer/components/dialogs/GeneralSettingsDialog.tsx
@@ -268,9 +268,9 @@ export default function GeneralSettingsDialog() {
             settingsKey="enableGamepad"
           />
           <SettingsDropdown
-            maxWidth={200}
-            label="Press-shortcut-again behavior"
-            info="This determines what happens when the shortcut is pressed again while a menu is shown. If multiple menus are assigned to the same shortcut, you can use this to cycle through all matching menus. Make sure to only hold down a modifier key if you want to use Turbo Mode together with an option other than 'Nothing'."
+            maxWidth={300}
+            label={i18next.t('settings.general-settings-dialog.press-again-behavior')}
+            info={i18next.t('settings.general-settings-dialog.press-again-behavior-info')}
             settingsKey="sameShortcutBehavior"
             options={[
               { value: 'nothing', label: 'Nothing' },
