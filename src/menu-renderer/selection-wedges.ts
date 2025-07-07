0c2b28c21613092500e735c9a1414a9bd26a1c08
Simon Schneegans <code@simonschneegans.de>
2025-07-07 06:45:28 +0200

    :memo: Add some comments

--- src/menu-renderer/selection-wedges.ts
+++ src/menu-renderer/selection-wedges.ts
@@ -10,10 +10,18 @@
 
 import { IVec2 } from '../common';
 
+/**
+ * The SelectionWedges class is responsible for rendering the background sections of the
+ * pie menu. It highlights the section that is currently hovered over and shows separators
+ * between the wedges.
+ *
+ * The separators are thin and long divs that are rotated to point towards the center of
+ * the pie menu. The highlighted wedge is drawn using a conic gradient in CSS.
+ */
 export class SelectionWedges {
   /**
-   * The div that contains the wedges. It covers the entire screen and contains a div for
-   * each separator between the wedges and a div for each wedge.
+   * The div that contains the separators and which also contains the conic gradient. It
+   * covers the entire screen.
    */
   private div: HTMLDivElement;
 
@@ -23,35 +31,50 @@ export class SelectionWedges {
    *
    * @param container The parent element of the wedges container.
    */
-  constructor(private container: HTMLElement) {
+  constructor(container: HTMLElement) {
     this.div = document.createElement('div');
     this.div.classList.add('wedges');
-    this.container.appendChild(this.div);
+    container.appendChild(this.div);
   }
 
-  /** This method shows the text element by removing the `hidden` class. */
+  /**
+   * Sets a new set of angles for the separators.
+   *
+   * @param angles An array of angles in degrees where the separators should be placed.
+   * @param position The position of the center of the pie menu in pixels.
+   */
   public setSeparators(angles: number[], position: IVec2) {
-    this.div.innerHTML = '';
-    this.div.style.setProperty('--center-x', `${position.x}px`);
-    this.div.style.setProperty('--center-y', `${position.y}px`);
+    const fragment = document.createDocumentFragment();
 
-    // We do not want a angle if there is only one wedge.
+    // We do not want a separator if there is only one wedge.
     if (angles.length > 1) {
       angles.forEach((angle) => {
         const div = document.createElement('div');
         div.className = 'separator';
         div.style.transform = `translate(${position.x}px, ${position.y}px) rotate(${angle - 90}deg)`;
-        this.div.appendChild(div);
+        fragment.appendChild(div);
       });
     }
+
+    this.div.innerHTML = '';
+    this.div.style.setProperty('--center-x', `${position.x}px`);
+    this.div.style.setProperty('--center-y', `${position.y}px`);
+    this.div.appendChild(fragment);
   }
 
+  /**
+   * Highlights a wedge by applying a conic gradient to the wedges container. The wedge is
+   * defined by its start and end angle in degrees.
+   *
+   * @param wedge An object containing the start and end angle of the wedge in degrees.
+   */
   public hover(wedge: { start: number; end: number }) {
     this.div.classList.add('hovered');
     this.div.style.setProperty('--start-angle', `${wedge.start}deg`);
     this.div.style.setProperty('--end-angle', `${wedge.end}deg`);
   }
 
+  /** Removes the highlight from the currently hovered wedge. */
   public unhover() {
     this.div.classList.remove('hovered');
   }
