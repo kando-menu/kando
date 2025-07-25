29c481829ba32cfb7d2e1727ded05569f48ea002
Simon Schneegans <code@simonschneegans.de>
2025-07-25 10:51:54 +0200

    :tada: Implement proper circle text wrapping

--- src/menu-renderer/center-text.ts
+++ src/menu-renderer/center-text.ts
@@ -14,177 +14,180 @@ import { IVec2 } from '../common';
  * This class is used to display the text in the center of the pie menu.
  *
  * Properly wrapping text inside a circular area is surprisingly difficult. It can be done
- * using the CSS `shape-outside` property, but this only works for floating elements. As a
- * consequence, it is impossible possible to vertically center the text inside the circle
- * using CSS only (see this for an explanation: https://stackoverflow.com/a/46228534). And
- * even with JavaScript this does not get much easier. It would require some kind of
- * iterative approach to find the ideal top padding for the text.
+ * using two half-circle `shape-outside` divs, but this only works for floating elements.
+ * As a consequence, it is difficult to vertically center the text inside the circle using
+ * CSS only (see this for an explanation: https://stackoverflow.com/a/46228534). Hence we
+ * have to use an iterative approach to find the top padding which optimally positions the
+ * text vertically.
  *
- * A potential approach would be to wrap the text in a square-shaped div instead. However,
- * this leads to excessive padding on the left and right side of the text. This looks
- * weird especially if the text would fit into one line.
- *
- * Instead, we wrap the text in a rectangular container. The width of the container is
- * determined by the number of lines required for the text. If there is only one line, the
- * div will be wider than if it has to wrap the text over multiple lines:
- *
- *             , - ~ ~ ~ - ,                                  , - ~ ~ ~ - ,
- *         , '               ' ,                          , '               ' ,
- *       ,                       ,                      ,  ,-----------------,  ,
- *      ,                         ,                    ,   |                 |   ,
- *     , ,-----------------------, ,                  ,    |                 |    ,
- *     , |                       | ,                  ,    |                 |    ,
- *     , '-----------------------' ,                  ,    |                 |    ,
- *      ,                         ,                    ,   |                 |   ,
- *       ,                       ,                      ,  '-----------------'  ,
- *         ,                  , '                         ,                  , '
- *           ' - , _ _ _ ,  '                               ' - , _ _ _ ,  '
- *
- * We will also ensure that the rectangle is always wider than it is high.
+ * We also use an iterative approach to reduce the cont size in case the entire text does
+ * not fit into the circle.
  *
  * Overall, this is not perfect, but it works well enough for our purposes. If anyone
  * comes up with a more elegant solution, please let me know! Also, this is not ideal
- * performance-wise as we have to query the bounding box of the text element multiple
- * times every time the text changes. We do this asynchronously using an
- * IntersectionObserver to avoid blocking the main thread, however, this is still not
- * ideal.
+ * performance-wise as we have to query the height of the text element multiple times
+ * every time the text changes. We do this asynchronously using an IntersectionObserver to
+ * avoid blocking the main thread, however, this is still not ideal.
  */
 export class CenterText {
   /**
-   * The div that contains the text. It will be positioned absolutely and translated
-   * relative to the center of the pie menu's root element.
+   * The div that contains the wrap-shapes and the inner div with the text. It will be
+   * positioned absolutely and translated relative to the center of the pie menu's root
+   * element.
    */
   private div: HTMLDivElement;
 
+  /**
+   * Since updating the text element is a rather expensive operation, we use an invisible
+   * staging div to compute the layout of the text element. Once the layout is computed,
+   * we replace the previous text element with the staging div.
+   */
+  private stagingDiv: HTMLDivElement;
+
   /** We query this once to compute the amount of lines which fit into the circle. */
-  private lineHeight: number = 0;
+  private maxFontSize: number = -1;
 
   /**
-   * The constructor creates the text element and appends it to the given container.
-   *
    * @param container The parent element of the text element.
-   * @param maxWidth The diameter of the circle in which the text is displayed.
+   * @param diameter The diameter of the circle in which the text is displayed.
    */
   constructor(
     private container: HTMLElement,
-    private maxWidth: number
-  ) {
-    this.div = this.createTextDiv();
-    this.container.appendChild(this.div);
-  }
+    private diameter: number
+  ) {}
 
-  /** This method shows the text element by removing the `hidden` class. */
-  public show() {
-    this.div.classList.remove('hidden');
-  }
-
-  /** This method hides the text element by adding the `hidden` class. */
+  /** Removes the current text from the container. */
   public hide() {
-    this.div.classList.add('hidden');
+    this.div?.remove();
   }
 
   /**
-   * This method sets the text of the text element. The method computes the width and
-   * height of the text element based on the text and the available space in the circle.
-   * The text is updated asynchronously, so it will not be updated immediately.
+   * Updates the center text. The method computes the optimum top padding to vertically
+   * center the text. The text is updated asynchronously, so it will not be updated
+   * immediately.
    *
    * @param text The text to display.
+   * @param position The position of the text element relative to the container. Aka the
+   *   current position of the pie menu on the screen.
    */
-  public async setText(text: string) {
-    // We query the line height only once for performance reasons.
-    if (this.lineHeight === 0) {
-      this.lineHeight = parseInt(window.getComputedStyle(this.div).lineHeight, 10);
+  public async show(text: string, position: IVec2) {
+    // If there is a staging div, a layout is already in progress. We remove it from the
+    // DOM so that it will not be used anymore.
+    if (this.stagingDiv) {
+      this.stagingDiv.remove();
+      this.stagingDiv = null;
     }
 
-    // We use a temporary div to compute the optimum width and height of the text element
-    // to avoid any flickering.
-    const tmpDiv = this.createTextDiv();
-    tmpDiv.textContent = text;
-    tmpDiv.style.visibility = 'hidden';
-    tmpDiv.style.position = 'absolute';
-    this.container.appendChild(tmpDiv);
+    // Computing the layout of the text is a rather expensive and slow operation. To avoid
+    // any flickering, we create a staging div that is not visible until the layout is
+    // computed. After the layout is computed, we remove the previous text element and
+    // make the staging div visible.
+    const stagingDiv = this.appendWithClass(this.container, 'center-text');
+    stagingDiv.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    stagingDiv.style.visibility = 'hidden';
+    stagingDiv.style.width = stagingDiv.style.height = `${this.diameter}px`;
+    stagingDiv.style.left = stagingDiv.style.top = `-${this.diameter / 2}px`;
 
-    // We only allow text areas with a aspect ratio >= 1.
-    const maxHeight = this.maxWidth / Math.SQRT2;
-    const maxLines = Math.floor(maxHeight / this.lineHeight);
+    // We keep a reference to the staging div so that we know that a layout is currently
+    // in progress if this method is called again before we are done.
+    this.stagingDiv = stagingDiv;
 
-    // We first assume that the text fits into one line. We compute the maximum available
-    // width for this line and then check if the text actually fits into this width. If
-    // not, we increase the number of lines and try again.
-    let targetLines = 1;
-    let width = 0;
-    let height = 0;
+    // Add the divs for wrapping the text inside a circle.
+    this.appendWithClass(stagingDiv, 'half-circle-wrap-left');
+    this.appendWithClass(stagingDiv, 'half-circle-wrap-right');
+    const textDiv = this.appendWithClass(stagingDiv, 'text');
 
-    while (targetLines <= maxLines) {
-      height = this.lineHeight * targetLines;
-      width = Math.sqrt(this.maxWidth * this.maxWidth - height * height);
+    const p = document.createElement('p');
+    p.textContent = text;
+    textDiv.appendChild(p);
 
-      tmpDiv.style.width = `${width}px`;
-      tmpDiv.style.height = 'auto';
+    // We query the initial font size only once for performance reasons.
+    if (this.maxFontSize === -1) {
+      this.maxFontSize = parseInt(window.getComputedStyle(p).fontSize, 10);
+    }
 
-      // Get number of lines required to fit the text.
-      const bounds = await this.getBoundingClientRectAsync(tmpDiv);
-      const actualLines = Math.floor(bounds.height / this.lineHeight);
+    // First, reduce the font size until everything fits.
+    const minFontSize = 8;
+    let fontSize = this.maxFontSize;
 
-      // If the text fits into the available space, we are done.
-      if (actualLines <= targetLines) {
-        break;
+    let textHeight = await this.getDivHeight(p);
+
+    while (textHeight > this.diameter && fontSize > minFontSize) {
+      fontSize = fontSize - 1;
+      p.style.fontSize = `${fontSize}px`;
+      textHeight = await this.getDivHeight(p);
+    }
+
+    // Now adjust the top margin of the text so that it is vertically centered in the circle.
+    if (textHeight < this.diameter) {
+      // Moving the text down so that the current height is centered in the circle
+      // may also provide more space for the text to grow left and right, so the
+      // text may fit on fewer lines. Hence check if the text height changed after
+      // adjusting the margin top.
+      let oldHeight = textHeight;
+      let oldMarginTop = 0;
+
+      for (let i = 0; i < 10; i++) {
+        const marginTop = (this.diameter - textHeight) / 2;
+        p.style.marginTop = `${(this.diameter - textHeight) / 2}px`;
+        textHeight = await this.getDivHeight(p);
+
+        // If the text height increased, we most likely moved the text too far. Let's
+        // take the previous margin value.
+        if (textHeight > oldHeight) {
+          p.style.marginTop = `${oldMarginTop}px`;
+          break;
+        }
+
+        // If the text height did not change, we are done.
+        if (textHeight === oldHeight) {
+          break;
+        }
+
+        oldHeight = textHeight;
+        oldMarginTop = marginTop;
       }
-
-      // Else we try again with one more line.
-      targetLines++;
     }
 
-    // We remove the temporary div again. There are cases where the container got cleared
-    // before the promise resolved, so we have to check if the div is still in the DOM.
-    if (tmpDiv.parentElement) {
-      this.container.removeChild(tmpDiv);
-
-      // We set the computed values to the actual text element.
-      this.div.textContent = text;
-      this.div.style.width = `${width}px`;
-      this.div.style.height = `${height}px`;
-      this.div.style.top = `${-height / 2}px`;
-      this.div.style.left = `${-width / 2}px`;
+    // If a new layout computation was started while the current one was still in
+    // progress, the staging was removed from the DOM. In this case, we do not need to
+    // continue.
+    if (stagingDiv.parentElement) {
+      this.hide();
+      this.div = stagingDiv;
+      this.div.style.visibility = 'initial';
+      this.stagingDiv = null;
     }
   }
 
   /**
-   * This method sets the position of the text element. The position is relative to the
-   * container element given in the constructor.
+   * This method returns the height of the given element. We use an IntersectionObserver
+   * to do this asynchronously.
    *
-   * @param position The position of the text element.
+   * @param element The element to get the height of.
+   * @returns A promise that resolves to the height of the element.
    */
-  public setPosition(position: IVec2) {
-    this.div.style.transform = `translate(${position.x}px, ${position.y}px)`;
-  }
-
-  /**
-   * This method creates the text element. The element is hidden by default.
-   *
-   * @returns The created text element.
-   */
-  private createTextDiv(): HTMLDivElement {
-    const div = document.createElement('div');
-    div.classList.add('center-text');
-    div.classList.add('hidden');
-    return div;
-  }
-
-  /**
-   * This method returns the bounding box of the given element. We use an
-   * IntersectionObserver to do this asynchronously.
-   *
-   * @param element The element to get the bounding box of.
-   * @returns A promise that resolves to the bounding box of the element.
-   */
-  private getBoundingClientRectAsync(element: HTMLElement): Promise<DOMRectReadOnly> {
+  private getDivHeight(element: HTMLElement): Promise<number> {
     return new Promise((resolve) => {
       const observer = new IntersectionObserver((entries) => {
         observer.disconnect();
-        resolve(entries[0].boundingClientRect);
+        resolve(entries[0].boundingClientRect.height);
       });
       observer.observe(element);
     });
+  }
+
+  /**
+   * Small helper to create div, add a class name and append it to the given container.
+   *
+   * @param container The element to attach the new element to.
+   * @param className The class to apply.
+   * @returns The newly created div.
+   */
+  private appendWithClass(container: HTMLElement, className: string) {
+    const element = document.createElement('div');
+    element.classList.add(className);
+    container.appendChild(element);
+    return element;
   }
 }
