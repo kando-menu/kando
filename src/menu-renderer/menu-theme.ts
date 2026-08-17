5bbd4cda1defc03ca7e297f36df458b4195de787
Simon Schneegans <code@simonschneegans.de>
2026-08-17 08:10:29 +0200

    :tada: Add parent-wedge properties

--- src/menu-renderer/menu-theme.ts
+++ src/menu-renderer/menu-theme.ts
@@ -41,7 +41,9 @@ export class MenuTheme {
 
   /**
    * Creates a new MenuTheme. This will register the custom CSS properties used by the
-   * theme.
+   * theme. These properties may change each frame, which is very expensive if the values
+   * are inherited. Therefore, we register them as custom properties which are not
+   * inherited.
    */
   constructor() {
     // Register the angular-difference CSS property. This is set each frame for each child
@@ -295,6 +297,31 @@ export class MenuTheme {
     if (hoverAngle != null) {
       item.renderData.lastHoveredChildAngle = hoverAngle;
     }
+  }
+
+  /**
+   * Sets the custom CSS properties for the given parent menu item. Currently, this is
+   * only the `--parent-wedge-start` and `--parent-wedge-end` properties which are set to
+   * the angular wedge towards the parent item.
+   *
+   * @param item The menu item to set the properties for.
+   * @param parentWedge The angular wedge towards the parent item.
+   */
+  public setParentProperties(
+    item: RenderedMenuItem,
+    parentWedge: {
+      start: number;
+      end: number;
+    }
+  ) {
+    item.renderData.nodeDiv.style.setProperty(
+      '--parent-wedge-start',
+      parentWedge.start + 'deg'
+    );
+    item.renderData.nodeDiv.style.setProperty(
+      '--parent-wedge-end',
+      parentWedge.end + 'deg'
+    );
   }
 
   /**
